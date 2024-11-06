b3bbe867f7b151a44a333b4267a131c4515bc46c
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-11-07 03:09:03 +0600

    Draw all keyboard symbols using map.

--- src/containers/DoorPassword/DoorPassword.tsx
+++ src/containers/DoorPassword/DoorPassword.tsx
@@ -1,5 +1,5 @@
 import {useDispatch, useSelector} from "react-redux";
-import {appendSymbol, removeSymbol} from "./doorPasswordSlice.ts";
+import {appendSymbol, removeSymbol, verifyDoorPassword} from "./doorPasswordSlice.ts";
 import {RootState} from "../../app/store.ts";
 
 
@@ -7,13 +7,22 @@ const DoorPassword = () => {
     const doorPasswordEnteredPassword = useSelector((state: RootState) => state.doorPassword.enteredPassword);
     const dispatch = useDispatch();
 
+    const symbols = ['9', '8', '7', '6', '5', '4', '3', '2', '1',  '<', '0', 'E'];
+
     return (
 
-        <div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
             {doorPasswordEnteredPassword}
+            <div></div>
             <hr/>
-            <button onClick={() => dispatch(appendSymbol(4))}>Add symbol</button>
-            <button className="ms-3" onClick={() => dispatch((removeSymbol()))}>Remove symbol</button>
+            <div className="row mb-4">
+                {symbols.map((symbol) => (
+                  <button className="m-2 w-25"  key={symbol} onClick={() => appendSymbol(symbol)}>{symbol}</button>
+            ))}
+            <button className="ms-2 mt-3 me-3 w-25" onClick={() => dispatch(appendSymbol('4'))}>Add symbol</button>
+            <button className="mt-3 me-3 w-25" onClick={() => dispatch((removeSymbol()))}>Remove symbol</button>
+            <button className="mt-3 me-3 w-25" onClick={() => dispatch((verifyDoorPassword()))} >Verify password</button>
+            </div>
         </div>
     );
 };
--- src/containers/DoorPassword/doorPasswordSlice.ts
+++ src/containers/DoorPassword/doorPasswordSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface doorPasswordState {
     enteredPassword: string;
@@ -13,17 +13,21 @@ export const doorPasswordSlice = createSlice({
     name: "doorPassword",
     initialState,
     reducers: {
-        appendSymbol: (state, action) => {
+        appendSymbol: (state, action: PayloadAction<string>) => {
             if (state.enteredPassword.length < 4) {
                 state.enteredPassword += action.payload;
             }
         },
         removeSymbol: (state) => {
             state.enteredPassword = state.enteredPassword.slice(0, -1);
-        }
+        },
+        verifyDoorPassword: (state) => {
+            const staticCorrectDoorPassword = '1503';
+            state.passwordIsSuitable = state.enteredPassword === staticCorrectDoorPassword;
+        },
     },
 });
 
 export const doorPasswordReducer = doorPasswordSlice.reducer;
 
-export const {appendSymbol, removeSymbol} = doorPasswordSlice.actions;
+export const {appendSymbol, removeSymbol, verifyDoorPassword} = doorPasswordSlice.actions;
